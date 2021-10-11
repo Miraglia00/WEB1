06bd4be7543fa60839bc52caa9d4c665eea2d269
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-10-11 19:30:55 +0200

    Added modal button to navbar, finished about.html content

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(() => {
-
-
+    var modal = new bootstrap.Modal($('#feedback_modal'), { backdrop: true })
+    var dom_modal = $('#feedback_modal')
     var toggled = false;
     var nav = $('#sidebar')
+    var headnav = $('#headbar')
+
+    dom_modal.remove()
 
     $('#toggle_small_nav').click(() => {
-        if(!toggled) {
+        if (!toggled) {
             $('*#nav_info').css('display', 'block')
             $('#toggle_icon').removeClass('bi-arrow-bar-right').addClass('bi-arrow-bar-left')
-        }else {
+        } else {
             $('*#nav_info').css('display', 'none')
             $('#toggle_icon').removeClass('bi-arrow-bar-left').addClass('bi-arrow-bar-right')
         }
@@ -16,23 +19,30 @@ $(document).ready(() => {
     })
 
     $(window).scroll(() => {
-        if($(window).scrollTop() >= 600){
-            console.log($(window).scrollTop())
+        if ($(window).scrollTop() >= 600) {
             $('.down').addClass('hidden')
-        }else{
+        } else {
             $('.down').removeClass('hidden')
         }
 
 
-        if(window.innerWidth >= 1600){
-            if($(window).scrollTop() >= 500) {
+        if (window.innerWidth >= 1600) {
+            if ($(window).scrollTop() >= 500) {
                 nav.css('display', 'block')
-            }else{
+                headnav.css('display', 'none')
+            } else {
                 nav.css('display', 'none')
+                headnav.css('display', 'block')
             }
         }
-        
-
     })
 
+    $('.open_modal_btn').click(() => {
+        dom_modal.insertAfter($('body'))
+        modal.show()
+    })
+
+    $(document).on('click', '#close_modal_btn', () => {
+        dom_modal.remove()
+    })
 })
